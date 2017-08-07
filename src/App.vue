79883692ae247c0c5ed0cc7694f3dd23e042b679
ziyal <wintermist3@gmail.com>
2017-08-07 09:49:12 -0700

    added navbar and images

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
   <div id="app">
+
+    <div class="header">
+
+      <h1>Sereina Charise Photography</h1>
+
+      <div id="navbar-container">
+        <ul class="navbar-list">
+          <li class="navbar-item"><router-link to="/">Home</router-link></li>
+          <li class="navbar-item"><router-link to="/albums">Albums</router-link></li>
+          <li class="navbar-item"><router-link to="/about">About</router-link></li>
+          <li class="navbar-item"><router-link to="/contact">Contact</router-link></li>
+        </ul>
+      </div>
+
+
+    </div>
+
+
     <router-view></router-view>
   </div>
 </template>
@@ -11,12 +29,60 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css?family=Open+Sans|Work+Sans:300');
+#navbar-container {
+  display: inline-block;
+  margin: 0 auto;
+}
+
+/* HEADER */
+
+h1 {
+  display: inline-block;
+  margin: 10px 0px 0px 15px;
+  padding-top: 10px;
+  width: 40%;
+  max-width: 500px;
+  font-size: 30px;
+  Font-Family: 'Work Sans', Sans-Serif;
+}
+
+.navbar-container {
+  display: inline-block;
+  width: 40%;
+}
+
+.navbar-list {
+  width: 600px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.navbar-item {
+  display: inline-block;
+  margin: 0px 20px 0px 20px;
+}
+
+/* DEFAULTS */
+a {
+  color: black;
+  text-decoration: none;
+}
+
+a:hover {
+  color: grey;
+  text-decoration: none;
+}
+
+* {
+  Font-Family: 'Open Sans', Sans-Serif;
+  Font-Size: 15px;
+}
+
+h2, h3, h4, h6 {
+  Font-Family: 'Work Sans', Sans-Serif;
+  Font-Size: 45px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="Home">
+
+    <div class="photos">
+
+      <ul>
+        <li>
+          <div class="category">
+            <h5>Nature</h5>
+          </div>
+          <img src="../assets/images/IMG_4992.jpg">
+        </li>
+
+        <li>
+          <img src="../assets/images/IMG_4215.jpg">
+        </li>
+
+        <li>
+          <img src="../assets/images/IMG_3946.jpg">
+        </li>
+
+        <li>
+          <img src="../assets/images/IMG_3812-2.jpg">
+        </li>
+
+      </ul>
+    </div>
+
+    <h2><router-link to="/admin">Admin Page</router-link></h2>
+    
+  </div>
+</template>
+
+<script>
+// export default {
+//   name: 'Home',
+//   data () {
+//     return {
+//       msg: 'Welcome to Your Vue.js App'
+//     }
+//   }
+// }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+
+li {
+  list-style-type: none;
+  display: inline-block;
+  // flex-direction: column;
+  background-color: black;
+  max-height: 300px;
+
+}
+
+.category {
+  z-index:100;
+  // display: none;
+  position: absolute;
+  // width: 300px;
+  text-align: center;
+  font-size: 40px;
+  color: white;
+}
+
+h5 {
+  text-align: center;
+  Font-Family: 'Work Sans', Sans-Serif;
+  Font-Size: 45px;
+}
+
+img {
+  max-width: 300px;
+  // background-color: black;
+  opacity: 0.9;
+  transition: 0.3s linear;
+   -webkit-transition: 0.3s linear;
+   -moz-transition: 0.3s linear;
+  
+}
+
+
+
+img:hover {
+  opacity: 1;
+}
+
+</style>
